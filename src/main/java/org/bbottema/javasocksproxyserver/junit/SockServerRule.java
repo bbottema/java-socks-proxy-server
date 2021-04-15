9a1e04f85ced771152f98d2b7a4e34372a2e7ee2
bbottema <b.bottema@projectnibble.org>
2021-04-15 17:06:25 +0200

    #4 add support for SSL connections in junit rule as well

--- src/main/java/org/bbottema/javasocksproxyserver/junit/SockServerRule.java
+++ src/main/java/org/bbottema/javasocksproxyserver/junit/SockServerRule.java
@@ -4,6 +4,8 @@ import org.bbottema.javasocksproxyserver.SocksServer;
 import org.jetbrains.annotations.NotNull;
 import org.junit.rules.ExternalResource;
 
+import javax.net.ServerSocketFactory;
+
 /**
  * Creates a {@link SocksServer} once, and starts and stops it before and after each test.
  * <p>
@@ -11,16 +13,23 @@ import org.junit.rules.ExternalResource;
  */
 public class SockServerRule extends ExternalResource {
 
-	private final SocksServer socksServer = new SocksServer();
+	private final SocksServer socksServer;
 	private final int port;
-
+	private final ServerSocketFactory serverSocketFactory;
+	
 	public SockServerRule(@NotNull Integer port) {
+		this(port, ServerSocketFactory.getDefault());
+	}
+	
+	public SockServerRule(@NotNull Integer port, @NotNull ServerSocketFactory serverSocketFactory) {
+		this.socksServer = new SocksServer();
 		this.port = port;
+		this.serverSocketFactory = serverSocketFactory;
 	}
 
 	@Override
 	protected void before() {
-		this.socksServer.start(port);
+		this.socksServer.start(port, serverSocketFactory);
 	}
 
 	@Override
